e8f4d5410b7c53e9d7ecbf428023639b212ff904
buzingga <nicoboisseau12@gmail.fr>
2021-02-02 09:59:42 +0100

    BaudRateGen from Thimon and Nico

--- src/BaudRateGen.cpp
+++ src/BaudRateGen.cpp
@@ -1,3 +1,25 @@
-#include <systemc.h>
 #include <iostream>
+#include <systemc.h>
 #include "BaudRateGen.hpp"
+
+baud_rate_generator::baud_rate_generator(sc_module_name nom) : sc_module(nom)
+{
+    SC_METHOD(divide_freq);
+    sensitive << mck << cd;
+}
+
+void baud_rate_generator::divide_freq()
+{
+	if(cd==0){
+		receiver_sapmling_clock.write(0);
+		baud_rate_clock.write(0);
+	}
+	else if(cd==1){
+		receiver_sapmling_clock.write(1);
+		baud_rate_clock.write(mck.read()/16);
+	}
+	else{
+		receiver_sapmling_clock.write(mck.read()/cd.read());
+		baud_rate_clock.write(mck.read()/cd.read()/16);
+	}
+}
--- src/BaudRateGen.hpp
+++ src/BaudRateGen.hpp
@@ -1,2 +1,13 @@
-#include <systemc.h>
 #include <iostream>
+#include <systemc.h>
+
+SC_MODULE(baud_rate_generator)
+{
+    sc_in<sc_uint<32> > mck;
+    sc_in<sc_uint<16> > cd;
+    sc_out<sc_uint<32> > baud_rate_clock, receiver_sapmling_clock;
+
+    SC_CTOR(baud_rate_generator);
+
+    void divide_freq();
+};

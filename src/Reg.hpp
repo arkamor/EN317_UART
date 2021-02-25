498ac3e91e7e3d73843e5eb387f4576eab76156a
BuildTools <unconfigured@null.spigotmc.org>
2021-02-25 11:57:51 +0100

    More & more functions from KP & VINCENT

--- src/Reg.hpp
+++ src/Reg.hpp
@@ -3,8 +3,7 @@
 //MAIN REGISTER
 //#define BASE_ADDR 0x400E0800
 
-#define REG_SIZE  32
-
+#define REG_SIZE 32
 
 //CONTROL REGISTER
 #define UART_CR_ADDR 0x00
@@ -18,7 +17,7 @@
 
 //MODE REGISTER
 #define UART_MR_ADDR 0x04
-#define PAR_Bp 9     //Parity Type : 0 EVEN - 1 ODD - 2 SPACE - 3 MARK - 4 NO
+#define PAR_Bp 9 //Parity Type : 0 EVEN - 1 ODD - 2 SPACE - 3 MARK - 4 NO
 
 //SATUS REGISTER
 #define UART_SR_ADDR 0x14
@@ -44,62 +43,114 @@
 class Reg
 {
 
-   public: Reg()
-    {
-        this->base_addr = reinterpret_cast<unsigned char*>(malloc(REG_SIZE));
-        std::cout << "Reg created" << std::endl;
-    }
+public:
+   Reg()
+   {
+      this->base_addr = reinterpret_cast<unsigned char *>(malloc(REG_SIZE));
+      std::cout << "Reg created" << std::endl;
+   }
 
-   public: void writeToRegister(sc_dt::uint64 offset, unsigned char *data, unsigned int len)
+public:
+   void writeToRegister(sc_dt::uint64 offset, unsigned char *data, unsigned int len)
    {
       printf("Wrote to register");
       memcpy(base_addr + offset, &data, len);
    }
 
-   public: void readRegister(sc_dt::uint64 offset, unsigned char *data, unsigned int len)
+public:
+   void readRegister(sc_dt::uint64 offset, unsigned char *data, unsigned int len)
    {
       memcpy(&data, base_addr + offset, len);
    }
 
-   public: unsigned char isRxEnable() 
+public:
+   void isIRQEn()
+   {
+      //return (*(base_addr + UART_CR_ADDR) >> RXEN_Bp) & 1;
+   }
+
+public:
+   unsigned char isRxEnable()
    {
       return (*(base_addr + UART_CR_ADDR) >> RXEN_Bp) & 1;
    }
 
-   public: unsigned char isTxEnable()
+public:
+   unsigned char isTxEnable()
    {
       return (*(base_addr + UART_CR_ADDR) >> TXEN_Bp) & 1;
    }
 
-   public: unsigned char isRxReady()
+public:
+   unsigned char isRxReady()
    {
       return (*(base_addr + UART_SR_ADDR) >> RXRDY_Bp) & 1;
    }
 
-   public: unsigned char isTxReady()
+public:
+   unsigned char isTxReady()
    {
       return (*(base_addr + UART_SR_ADDR) >> TXRDY_Bp) & 1;
    }
 
-   public: void resetTxReady()
+public:
+   void resetTxReady()
    {
-      *(base_addr + UART_SR_ADDR) = *(base_addr + UART_SR_ADDR) & (1 << TXRDY_Bp);
+      *(base_addr + UART_SR_ADDR) = *(base_addr + UART_SR_ADDR) | (1 << TXRDY_Bp); 
    }
 
-   public: void writeRHR(unsigned char data)
+public:
+   void setRxReady()
+   {
+      *(base_addr + UART_SR_ADDR) |= (1 << RXRDY_Bp); 
+   }
+
+public:
+   void disableTx()
+   {
+      *(base_addr + UART_CR_ADDR) &= ~(1 << TXEN_Bp);
+      *(base_addr + UART_CR_ADDR) |= (1 << TXDIS_Bp);
+   }
+
+public:
+   void disableRx()
+   {
+      *(base_addr + UART_CR_ADDR) &= ~(1 << RXEN_Bp);
+      *(base_addr + UART_CR_ADDR) |= (1 << RXDIS_Bp);
+   }
+
+public:
+   void enableTx()
+   {
+      *(base_addr + UART_CR_ADDR) &= ~(1 << TXDIS_Bp);
+      *(base_addr + UART_CR_ADDR) |= (1 << TXEN_Bp);
+   }
+
+public:
+   void enableRx()
+   {
+      *(base_addr + UART_CR_ADDR) &= ~(1 << RXDIS_Bp);
+      *(base_addr + UART_CR_ADDR) |= (1 << RXEN_Bp);
+   }
+
+public:
+   void writeRHR(unsigned char data)
    {
       *(base_addr + UART_RHR_ADDR) = *(base_addr + UART_RHR_ADDR) & (data << RXCHR_Bp));
    }
 
-   public: unsigned char readTHR()
+public:
+   unsigned char readTHR()
    {
       return *(base_addr + UART_THR_ADDR) & (1 << TXCHR_Bp);
    }
 
-   public: unsigned char readyParity()
+public:
+   unsigned char readyParity()
    {
       return (*(base_addr + UART_MR_ADDR) >> PAR_Bp) & 0x07;
    }
 
-   private: unsigned char *base_addr;
+private:
+   unsigned char *base_addr;
 };
